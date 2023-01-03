16592278099d637649e69162994a140ce6c1c7c0
ChilliDee Design <chillideedesign@ALCMAC02-iMac.local>
2023-01-04 07:44:48 +1100

    first-commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import logo from "../images/logo.svg";
+import { pageList } from "../data";
+import { socialLinks } from "../data";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          {pageList.map((item) => {
+            const { href, text, id } = item;
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            const { icon, id, href } = link;
+
+            return (
+              <li key={id}>
+                <a
+                  href={link}
+                  target="_blank"
+                  className="nav-icon"
+                  rel="noreferrer"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const pageList = [
+  {
+    id: 1,
+    href: "#home",
+    text: "Home",
+  },
+  {
+    id: 2,
+    href: "#About",
+    text: "About",
+  },
+  {
+    id: 3,
+    href: "#Services",
+    text: "Services",
+  },
+  {
+    id: 4,
+    href: "#Tours",
+    text: "Tours",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.facebook.com",
+    icon: "fab fa-facebook",
+  },
+  {
+    id: 2,
+    href: "https://www.twitter.com",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 1,
+    href: "https://www.squarespace.com",
+    icon: "fab fa-squarespace",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing eli. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
